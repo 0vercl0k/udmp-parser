6bcc9f1d3aeba8359c5bea6f2fa3552990be67ac
Axel Souchet <1476421+0vercl0k@users.noreply.github.com>
2023-06-27 20:05:47 -0700

    Remove exception, move the debug printf to the debug configuration and fix memory corruption (#13)

--- src/python/tests/test_parser.py
+++ src/python/tests/test_parser.py
@@ -19,6 +19,7 @@ import unittest
 # format: on
 import udmp_parser  # type: ignore
 from .utils import generate_minidump_from_process_name  # type: ignore
+
 # format: off
 
 TARGET_PROCESS_NAME: str = "winver.exe"
@@ -124,6 +125,24 @@ class TestParserBasic(unittest.TestCase):
         memory_regions = parser.Memory()
         assert len(memory_regions)
 
+    def test_memory_inexistent(self):
+        """This ensures that `ReadMemory` returns `None` when trying to
+        read a segment of memory that isn't described in the dump file."""
+        parser = udmp_parser.UserDumpParser()
+        assert parser.Parse(self.minidump_file)
+        assert parser.ReadMemory(0xDEADBEEF_BAADC0DE, 0x10) is None
+
+    def test_memory_empty(self):
+        """This ensures that `ReadMemory` returns an empty array (and not `None`)
+        when trying to read into a memory region that has no data associated."""
+        parser = udmp_parser.UserDumpParser()
+        assert parser.Parse(self.minidump_file)
+        mem = parser.Memory()
+        empty_regions = list(filter(lambda m: m.DataSize == 0, mem.values()))
+        assert len(empty_regions) > 0
+        empty_region = empty_regions[0]
+        assert len(parser.ReadMemory(empty_region.BaseAddress, 0x10)) == 0
+
     def test_utils(self):
         assert udmp_parser.utils.TypeToString(0x2_0000) == "MEM_PRIVATE"
         assert udmp_parser.utils.TypeToString(0x4_0000) == "MEM_MAPPED"
@@ -140,11 +159,15 @@ class TestParserBasic(unittest.TestCase):
         assert udmp_parser.utils.ProtectionToString(0x04) == "PAGE_READWRITE"
         assert udmp_parser.utils.ProtectionToString(0x08) == "PAGE_WRITECOPY"
         assert udmp_parser.utils.ProtectionToString(0x10) == "PAGE_EXECUTE"
-        assert udmp_parser.utils.ProtectionToString(0x20) == "PAGE_EXECUTE_READ"
-        assert udmp_parser.utils.ProtectionToString(0x40) == "PAGE_EXECUTE_READWRITE"
-        assert udmp_parser.utils.ProtectionToString(0x80) == "PAGE_EXECUTE_WRITECOPY"
+        assert udmp_parser.utils.ProtectionToString(
+            0x20) == "PAGE_EXECUTE_READ"
+        assert udmp_parser.utils.ProtectionToString(
+            0x40) == "PAGE_EXECUTE_READWRITE"
+        assert udmp_parser.utils.ProtectionToString(
+            0x80) == "PAGE_EXECUTE_WRITECOPY"
         assert (
-            udmp_parser.utils.ProtectionToString(0x18) == "PAGE_WRITECOPY,PAGE_EXECUTE"
+            udmp_parser.utils.ProtectionToString(
+                0x18) == "PAGE_WRITECOPY,PAGE_EXECUTE"
         )
         assert (
             udmp_parser.utils.ProtectionToString(0x19)
